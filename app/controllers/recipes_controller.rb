45f0f10e256a0ceb964842fbb9f5d998d4d17cab
Bob Roberts <cardsfanbob@gmail.com>
2017-09-08 23:40:52 -0500

    added wil paginate for the max list of 10 recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
+  require 'will_paginate/array'
 
   def index
     if params['query'].present?
-      @recipes = Recipe.search(params['query'])
+      @recipes = Recipe.search(params['query']).paginate(:page => params[:page], :per_page => 10)
     end
   end
 
